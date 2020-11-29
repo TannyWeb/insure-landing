6e8df180300e43c1255e295bf0aed2bec258751d
Tanvir Ali <Tanvir.Ali@dentsu.com>
2020-11-29 22:00:08 +0000

    rm all of un-needed files from old test

--- code/src/js/functions/generateDOM.js
+++
@@ -1,9 +0,0 @@
-export const addUserToDOM = (htmlSting, className) => {
-	const el = document.createElement('div');
-	el.classList.add(`wrapper`);
-	el.classList.add(`wrapper-${className}`);
-
-	el.innerHTML = htmlSting;
-
-	document.querySelector('body').appendChild(el);
-};
